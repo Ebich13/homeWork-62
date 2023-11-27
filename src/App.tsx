b17e6b7450a8181db724425c199348babaaf408c
Kubat <kubas9214@gmail.com>
2023-11-27 16:48:03 +0600

    Added application logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-function App() {
+import React from 'react';
+import {Route, Routes,} from 'react-router-dom';
+import Home from './container/Home/Home';
+import AboutUs from './container/AboutUs/AboutUs';
+import Contacts from './container/Contacts/Contacts';
+import Navigation from "./components/Navigation/Navigation";
 
+function App() {
   return (
     <>
-      <div>
-        Test
-      </div>
+      <header>
+        <Navigation/>
+      </header>
+      <main className="containd-fluid">
+        <Routes>
+          <Route path="/" element={(
+            <Home/>
+          )}/>
+          <Route path="/about-us" element={(
+            <AboutUs/>
 
+          )} />
+          <Route path="/contacts" element={(
+            <Contacts />
+          )}/>
+          <Route path="*" element={(<h1>Content will be soon!</h1>)}/>
+        </Routes>
+      </main>
     </>
   );
 }
+
 
 export default App;
